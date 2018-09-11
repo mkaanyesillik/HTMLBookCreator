23caebf1c40031c9b4964d2933fdb83fb660c8cd
Kaan Yeşillik <mkaanyesillik@gmail.com>
2018-09-11 21:02:27 +0300

    Initial commit

---
+++ HtmlBookCreator/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace HtmlBookCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKopyala = new System.Windows.Forms.Button();
+            this.btnSayfaEkle = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtSayfaSayisi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnIceAktar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKopyala
+            // 
+            this.btnKopyala.Location = new System.Drawing.Point(218, 487);
+            this.btnKopyala.Name = "btnKopyala";
+            this.btnKopyala.Size = new System.Drawing.Size(200, 56);
+            this.btnKopyala.TabIndex = 0;
+            this.btnKopyala.Text = "Kopyala";
+            this.btnKopyala.UseVisualStyleBackColor = true;
+            this.btnKopyala.Click += new System.EventHandler(this.btnKopyala_Click);
+            // 
+            // btnSayfaEkle
+            // 
+            this.btnSayfaEkle.Location = new System.Drawing.Point(12, 487);
+            this.btnSayfaEkle.Name = "btnSayfaEkle";
+            this.btnSayfaEkle.Size = new System.Drawing.Size(200, 56);
+            this.btnSayfaEkle.TabIndex = 1;
+            this.btnSayfaEkle.Text = "Sayfa Ekle";
+            this.btnSayfaEkle.UseVisualStyleBackColor = false;
+            this.btnSayfaEkle.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.GreenYellow;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(816, 420);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtSayfaSayisi
+            // 
+            this.txtSayfaSayisi.Location = new System.Drawing.Point(105, 438);
+            this.txtSayfaSayisi.MaxLength = 4;
+            this.txtSayfaSayisi.Name = "txtSayfaSayisi";
+            this.txtSayfaSayisi.Size = new System.Drawing.Size(313, 22);
+            this.txtSayfaSayisi.TabIndex = 3;
+            this.txtSayfaSayisi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSayfaSayisi_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 441);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sayfa Sayısı :";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(424, 487);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(200, 56);
+            this.btnKaydet.TabIndex = 5;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnIceAktar
+            // 
+            this.btnIceAktar.Location = new System.Drawing.Point(631, 487);
+            this.btnIceAktar.Name = "btnIceAktar";
+            this.btnIceAktar.Size = new System.Drawing.Size(200, 56);
+            this.btnIceAktar.TabIndex = 6;
+            this.btnIceAktar.Text = "Sayfaları İçe Aktar";
+            this.btnIceAktar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 555);
+            this.Controls.Add(this.btnIceAktar);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSayfaSayisi);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSayfaEkle);
+            this.Controls.Add(this.btnKopyala);
+            this.Name = "Form1";
+            this.Text = "HTML Kitap Oluşturucu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKopyala;
+        private System.Windows.Forms.Button btnSayfaEkle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtSayfaSayisi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnIceAktar;
+    }
+}
+
